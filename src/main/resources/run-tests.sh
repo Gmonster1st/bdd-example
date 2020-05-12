07366ea686e51ee862b7ec7243214068a910d0a1
PETROU Vasilios <vpetrou@intrasoft-intl.com>
2020-05-12 16:39:55 +0300

    update cmd runner with headless mode option

--- src/main/resources/run-tests.sh
+++ src/main/resources/run-tests.sh
@@ -4,12 +4,13 @@ ENVIRONMENT=prefat
 BROWSER=chrome
 DRIVER_REMOTE_URL=
 TIMEOUT=15
+HEADLESS_MODE=false
 
 # DEFAULT OPTION: Full Execution
 if [ "$1" == "" ]; then
 	echo "Execution of ALL Tests in $ENVIRONMENT with $BROWSER"
 	cd ../../../
-	mvn clean verify -Denvironment=$ENVIRONMENT -Dbrowser=$BROWSER -Ddriver.remote.url=$DRIVER_REMOTE_URL -Dtimeout=$TIMEOUT
+	mvn clean verify -Denvironment=$ENVIRONMENT -Dbrowser=$BROWSER -Ddriver.remote.url=$DRIVER_REMOTE_URL -Dtimeout=$TIMEOUT -Dheadless.mode=$HEADLESS_MODE
   exit
 fi
 
@@ -21,6 +22,6 @@ fi
 if [ "$1" != "" ]; then
 	echo "Execution of Test: $1 in $ENVIRONMENT with $BROWSER"
 	cd ../../../
-	mvn clean install -Dcucumber.options="--tags @$1" -Denvironment=$ENVIRONMENT -Dbrowser=$BROWSER -Ddriver.remote.url=$DRIVER_REMOTE_URL -Dtimeout=$TIMEOUT
+	mvn clean install -Dcucumber.options="--tags @$1" -Denvironment=$ENVIRONMENT -Dbrowser=$BROWSER -Ddriver.remote.url=$DRIVER_REMOTE_URL -Dtimeout=$TIMEOUT -Dheadless.mode=$HEADLESS_MODE
 	exit
 fi
